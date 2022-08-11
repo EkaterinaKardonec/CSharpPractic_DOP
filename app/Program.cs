6f2b4bbe2a4ccd4ffef879c2336e6e9d72b79c25
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-11 11:46:38 +0700

    вывод числа, с точнотью до 100х

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,13 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double str;
+            Console.WriteLine("Введите число используя знак - ',' ");
+            str = Convert.ToDouble(Console.ReadLine());
+            
+            double a = Convert.ToDouble(str);
+            Console.WriteLine("Вы ввели число " + str);
+
         }
     }
 }
